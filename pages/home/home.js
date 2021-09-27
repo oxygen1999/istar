83b0ec8b00a3c4a39affaebe86ceabadea28f3c1
逍遥散人 <1345314891@qq.com>
2021-09-27 15:59:27 +0800

    新增了一些测试内容

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,21 +11,51 @@ Page({
   // ***********路由跳转****************
   // **********************************
   // 跳转折叠面板
-  jumpToFold(){
+  jumpToFold() {
     wx.navigateTo({
       url: './../istarFold/istarFold',
     })
   },
   // 跳转底部固定面板
-  jumpToBottom(){
+  jumpToBottom() {
     wx.navigateTo({
       url: './../istarBottom/istarBottom',
     })
   },
   // 跳转通告面板
-  jumpToNotify(){
+  /** */
+
+  jumpToNotify() {
     wx.navigateTo({
       url: './../istarNotify/istarNotify',
+    })
+  },
+  /**
+   * 跳转进度条面板
+   * @param {*} options 
+   */
+  jumpToProcess() {
+    wx.navigateTo({
+      url: '/pages/istarProcess/istarProcess',
+    })
+
+  },
+  /**
+     * 跳转打开pdf
+     */
+  jumpToPDF() {
+    wx.navigateTo({
+      url: '/pages/istarPDF/istarPDF',
+    })
+
+  },
+  /**
+   * markdown 测试
+   * @param {*} options 
+   */
+  jumpMD(){
+    wx.navigateTo({
+      url: '/pages/istarMD/istarMD',
     })
   },
   // ===========分割线============
